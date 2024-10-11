f5fd35a53b3bfb8f9d79c22b0cb6f20806902c66
Sung-99 <vitorhugomota99@hotmail.com>
2024-10-11 06:19:27 -0300

    up .env removed

--- src/tsFilesRunner/ExecutingTs.ts
+++ src/tsFilesRunner/ExecutingTs.ts
@@ -1,4 +1,4 @@
 let namee: string = "Vitor";
-
+let token: string = process.env.GOOGLE_DRIVE_TOKEN as string;
 console.log(`meu nome é ${namee}, gostoso`);
-console.log(`Token google: ${process.env.GOOGLE_DRIVE_TOKEN}`);
+console.log(`Token google: ${token}`);
