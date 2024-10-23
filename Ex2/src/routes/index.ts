b46c5d0f8fe3d63da7b9641a7d4aaec9c546072d
Sung-99 <vitorhugomota99@hotmail.com>
2024-10-23 06:25:41 -0300

    up

--- Ex2/src/routes/index.ts
+++ Ex2/src/routes/index.ts
@@ -1,5 +1,29 @@
 import express from 'express';
+import  { readFile, writeFile  } from 'fs/promises';
 
+const dataSource = './data/list.txt'
 const router = express.Router();
+
+
+router.post('/contato', async (req, res) => {
+    const { name } = req.body;
+    
+    if (!name || name.length < 2) {
+         res.json({error: 'name need at least 2 character'});
+         return;
+    }
+    
+    //data process
+
+    let list: string[] = [];
+    try{
+        const data = await readFile(dataSource, {encoding: 'utf8'});
+        list = data.split('\n');
+    }catch (err){}
+    list.push(name);
+    await writeFile(dataSource, list.join('\n'))
+
+    res.status(201).json({contato: name});
+});
 
 export default router;
--- Ex2/src/server.ts
+++ Ex2/src/server.ts
@@ -14,4 +14,4 @@ server.use('/', router);
 server.listen(3000, () => {
 
     console.log('server running http://localhost:3000');
-})
+});
