74d1856d90d9d8ee6d2b69a5cf561cf00ee53f4c
Sung-99 <vitorhugomota99@hotmail.com>
2024-10-26 07:40:49 -0300

    up

--- Ex2/src/routes/index.ts
+++ Ex2/src/routes/index.ts
@@ -39,4 +39,21 @@ router.get('/contatos', async(req, res) =>{
     res.json({contatos: list});
 });
 
+router.delete('/contatos', async (req, res) => {
+    const { name } = req.query;
+    if(!name){
+       res.json({error: "No name inserted"});
+    }
+    let list: string[] = [];
+    try{
+
+        const data = await readFile(dataSource, {encoding: 'utf8'});
+        list = data.split('\n');
+    }catch(err){}
+    list = list.filter(item => item.toLowerCase() !== (name as string).toLowerCase());
+    
+    await writeFile(dataSource, list.join('\n'));
+    res.json({contato: name});
+});
+
 export default router;
